5aebdb5e4265d38a47d7d7b72ccd0808ae042926
michaelzon <michael.zonneveld@live.nl>
2018-06-07 13:21:42 +0200

    yoyo

--- PROCESS.md
+++ PROCESS.md
@@ -1,7 +1,15 @@
 # dag 1
 
-* Gisteravond heb ik mijn datasets wat aangepast: ik neem minder variabelen over het internetgebruik in Nederland. Heb twee extra datasets geimporteert via statline: eentje over politieke interesse en eentje over politieke participatie. Zo sluiten de variabelen van mijn visualisaties beter op elkaar aan. 
+* Gisteravond heb ik mijn datasets wat aangepast: ik neem minder variabelen over het internetgebruik in Nederland. Heb twee extra datasets geimporteert via statline: eentje over politieke interesse en eentje over politieke participatie. Zo sluiten de variabelen van mijn visualisaties beter op elkaar aan.
 
 * Vandaag ga ik beginnen aan de prototype.
 
 * Ik hergebruik mijn bootstrap code van de opdracht Linked Views.
+
+* Er is een werkende navigatiebalk!
+
+* Ik heb twee canvassen aangemaakt voor de drie visualisaties. Ik heb het skelet van de margin convention erbij geimporteert want als iets een struikelblok was en ongelooflijk irritant dan was het padden van visualisaties. Nu zie ik precies waar ik "ben" op mijn website.
+
+![](photo/image_3.jpg)
+
+![](photo/image_4.jpg)
--- javascript/vertrouwen.js
+++ javascript/vertrouwen.js
@@ -2,32 +2,281 @@
 
 window.onload = function(){
 
-  // api request for the data
-  var rawTableInfo = "https://opendata.cbs.nl/ODataApi/odata/82378NED/TableInfos";
-  var rawData = "https://opendata.cbs.nl/ODataApi/odata/82378NED/TypedDataSet";
-  var rawProperties = "https://opendata.cbs.nl/ODataApi/odata/82378NED/DataProperties";
-  var rawGroups = "https://opendata.cbs.nl/ODataApi/odata/82378NED/Persoonskenmerken";
-  var rawPeriods = "https://opendata.cbs.nl/ODataApi/odata/82378NED/Perioden";
+  // // api request for the data
+  // var rawTableInfo = "https://opendata.cbs.nl/ODataApi/odata/82378NED/TableInfos";
+  // var rawData = "https://opendata.cbs.nl/ODataApi/odata/82378NED/TypedDataSet";
+  // var rawProperties = "https://opendata.cbs.nl/ODataApi/odata/82378NED/DataProperties";
+  // var rawGroups = "https://opendata.cbs.nl/ODataApi/odata/82378NED/Persoonskenmerken";
+  // var rawPeriods = "https://opendata.cbs.nl/ODataApi/odata/82378NED/Perioden";
+  //
+  // // request for the queries
+  // d3.queue()
+  // .defer(d3.request, rawTableInfo)
+  // .defer(d3.request, rawData)
+  // .defer(d3.request, rawProperties)
+  // .defer(d3.request, rawGroups)
+  // .defer(d3.request, rawPeriods)
+  // .awaitAll(doSomething);
 
-  // request for the queries
-  d3.queue()
-  .defer(d3.request, rawTableInfo)
-  .defer(d3.request, rawData)
-  .defer(d3.request, rawProperties)
-  .defer(d3.request, rawGroups)
-  .defer(d3.request, rawPeriods)
-  .awaitAll(doSomething);
-
-  console.log(rawData)
+  makeLinegraphCanvas()
+  makeDendrogramCanvas()
 
 };
 
-function doSomething(error, response){
+// function doSomething(error, response){
+//
+//   // check if data gets loaded
+//   if (error) throw error;
+//
+//   var tableInfo = JSON.parse(response[0].responseText);
+//   var data = JSON.parse(response[1].responseText);
+//   console.log(data)
+// };
 
-  // check if data gets loaded
-  if (error) throw error;
+function makeLinegraphCanvas(){
+  var margin = {top: 20, right: 20, bottom: 20, left: 20},
+      padding = {top: 60, right: 60, bottom: 60, left: 60},
+      outerWidth = 1260,
+      outerHeight = 700,
+      innerWidth = outerWidth - margin.left - margin.right,
+      innerHeight = outerHeight - margin.top - margin.bottom,
+      width = innerWidth - padding.left - padding.right,
+      height = innerHeight - padding.top - padding.bottom;
 
-  var tableInfo = JSON.parse(response[0].responseText);
-  var data = JSON.parse(response[1].responseText);
-  console.log(data)
+  var x = d3.scaleIdentity()
+      .domain([0, width]);
+
+  var y = d3.scaleIdentity()
+      .domain([0, height]);
+
+  var xAxis = d3.axisBottom()
+      .scale(x)
+
+  var yAxis = d3.axisRight()
+      .scale(y)
+
+  var svg = d3.select("#linegraph").append("svg")
+      .attr("width", outerWidth)
+      .attr("height", outerHeight)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var defs = svg.append("defs");
+
+  defs.append("marker")
+      .attr("id", "triangle-start")
+      .attr("viewBox", "0 0 10 10")
+      .attr("refX", 10)
+      .attr("refY", 5)
+      .attr("markerWidth", -6)
+      .attr("markerHeight", 6)
+      .attr("orient", "auto")
+    .append("path")
+      .attr("d", "M 0 0 L 10 5 L 0 10 z");
+
+  defs.append("marker")
+      .attr("id", "triangle-end")
+      .attr("viewBox", "0 0 10 10")
+      .attr("refX", 10)
+      .attr("refY", 5)
+      .attr("markerWidth", 6)
+      .attr("markerHeight", 6)
+      .attr("orient", "auto")
+    .append("path")
+      .attr("d", "M 0 0 L 10 5 L 0 10 z");
+
+  svg.append("rect")
+      .attr("class", "outer")
+      .attr("width", innerWidth)
+      .attr("height", innerHeight);
+
+  var g = svg.append("g")
+      .attr("transform", "translate(" + padding.left + "," + padding.top + ")");
+
+  g.append("rect")
+      .attr("class", "inner")
+      .attr("width", width)
+      .attr("height", height);
+
+  g.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  g.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + width + ",0)")
+      .call(yAxis);
+
+  svg.append("line")
+      .attr("class", "arrow")
+      .attr("x2", padding.left)
+      .attr("y2", padding.top)
+      .attr("marker-end", "url(#triangle-end)");
+
+  svg.append("line")
+      .attr("class", "arrow")
+      .attr("x1", innerWidth / 2)
+      .attr("x2", innerWidth / 2)
+      .attr("y2", padding.top)
+      .attr("marker-end", "url(#triangle-end)");
+
+  svg.append("line")
+      .attr("class", "arrow")
+      .attr("x1", innerWidth / 2)
+      .attr("x2", innerWidth / 2)
+      .attr("y1", innerHeight - padding.bottom)
+      .attr("y2", innerHeight)
+      .attr("marker-start", "url(#triangle-start)");
+
+  svg.append("line")
+      .attr("class", "arrow")
+      .attr("x2", padding.left)
+      .attr("y1", innerHeight / 2)
+      .attr("y2", innerHeight / 2)
+      .attr("marker-end", "url(#triangle-end)");
+
+  svg.append("line")
+      .attr("class", "arrow")
+      .attr("x1", innerWidth)
+      .attr("x2", innerWidth - padding.right)
+      .attr("y1", innerHeight / 2)
+      .attr("y2", innerHeight / 2)
+      .attr("marker-end", "url(#triangle-end)");
+
+  svg.append("text")
+      .text("origin")
+      .attr("y", -8);
+
+    svg.append("circle")
+        .attr("class", "origin")
+        .attr("r", 4.5);
+
+  g.append("text")
+      .text("translate(margin.left, margin.top)")
+      .attr("y", -8);
+};
+
+function makeDendrogramCanvas(){
+  var margin = {top: 20, right: 20, bottom: 20, left: 20},
+      padding = {top: 60, right: 60, bottom: 60, left: 60},
+      outerWidth = 1260,
+      outerHeight = 1000,
+      innerWidth = outerWidth - margin.left - margin.right,
+      innerHeight = outerHeight - margin.top - margin.bottom,
+      width = innerWidth - padding.left - padding.right,
+      height = innerHeight - padding.top - padding.bottom;
+
+  var x = d3.scaleIdentity()
+      .domain([0, width]);
+
+  var y = d3.scaleIdentity()
+      .domain([0, height]);
+
+  var xAxis = d3.axisBottom()
+      .scale(x)
+
+  var yAxis = d3.axisRight()
+      .scale(y)
+
+  var svg = d3.select("#dendrogram").append("svg")
+      .attr("width", outerWidth)
+      .attr("height", outerHeight)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var defs = svg.append("defs");
+
+  defs.append("marker")
+      .attr("id", "triangle-start")
+      .attr("viewBox", "0 0 10 10")
+      .attr("refX", 10)
+      .attr("refY", 5)
+      .attr("markerWidth", -6)
+      .attr("markerHeight", 6)
+      .attr("orient", "auto")
+    .append("path")
+      .attr("d", "M 0 0 L 10 5 L 0 10 z");
+
+  defs.append("marker")
+      .attr("id", "triangle-end")
+      .attr("viewBox", "0 0 10 10")
+      .attr("refX", 10)
+      .attr("refY", 5)
+      .attr("markerWidth", 6)
+      .attr("markerHeight", 6)
+      .attr("orient", "auto")
+    .append("path")
+      .attr("d", "M 0 0 L 10 5 L 0 10 z");
+
+  svg.append("rect")
+      .attr("class", "outer")
+      .attr("width", innerWidth)
+      .attr("height", innerHeight);
+
+  var g = svg.append("g")
+      .attr("transform", "translate(" + padding.left + "," + padding.top + ")");
+
+  g.append("rect")
+      .attr("class", "inner")
+      .attr("width", width)
+      .attr("height", height);
+
+  g.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  g.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + width + ",0)")
+      .call(yAxis);
+
+  svg.append("line")
+      .attr("class", "arrow")
+      .attr("x2", padding.left)
+      .attr("y2", padding.top)
+      .attr("marker-end", "url(#triangle-end)");
+
+  svg.append("line")
+      .attr("class", "arrow")
+      .attr("x1", innerWidth / 2)
+      .attr("x2", innerWidth / 2)
+      .attr("y2", padding.top)
+      .attr("marker-end", "url(#triangle-end)");
+
+  svg.append("line")
+      .attr("class", "arrow")
+      .attr("x1", innerWidth / 2)
+      .attr("x2", innerWidth / 2)
+      .attr("y1", innerHeight - padding.bottom)
+      .attr("y2", innerHeight)
+      .attr("marker-start", "url(#triangle-start)");
+
+  svg.append("line")
+      .attr("class", "arrow")
+      .attr("x2", padding.left)
+      .attr("y1", innerHeight / 2)
+      .attr("y2", innerHeight / 2)
+      .attr("marker-end", "url(#triangle-end)");
+
+  svg.append("line")
+      .attr("class", "arrow")
+      .attr("x1", innerWidth)
+      .attr("x2", innerWidth - padding.right)
+      .attr("y1", innerHeight / 2)
+      .attr("y2", innerHeight / 2)
+      .attr("marker-end", "url(#triangle-end)");
+
+  svg.append("text")
+      .text("origin")
+      .attr("y", -8);
+
+    svg.append("circle")
+        .attr("class", "origin")
+        .attr("r", 4.5);
+
+  g.append("text")
+      .text("translate(margin.left, margin.top)")
+      .attr("y", -8);
 };

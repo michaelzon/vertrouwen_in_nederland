934f0b20db02871070a0623c3a503968377eda42
michaelzon <michael.zonneveld@live.nl>
2018-06-13 18:24:08 +0200

    done for today, data reconstruction is now complete and very clean

--- javascript/importData.js
+++ javascript/importData.js
@@ -16,18 +16,6 @@ function importData(error, response){
 
   var vertrouwen = (bigDatty[0]["vertrouwen"]);
 
-  // for(var i = 0; i < vertrouwen.length; i ++){
-  //   vertrouwen[i].ID = Number(vertrouwen[i].ID); // don't forget to store number value in current value
-  //   vertrouwen[i].Periode = Number(vertrouwen[i].Periode);
-  //   vertrouwen[i].Ambtenaren = Number(vertrouwen[i].Ambtenaren);
-  //   vertrouwen[i].EuropeseUnie = Number(vertrouwen[i].EuropeseUnie);
-  //   vertrouwen[i].Pers = Number(vertrouwen[i].Pers);
-  //   vertrouwen[i].Politie = Number(vertrouwen[i].Politie);
-  //   vertrouwen[i].Rechters = Number(vertrouwen[i].Rechters);
-  //   vertrouwen[i].TweedeKamer = Number(vertrouwen[i].TweedeKamer);
-  //   vertrouwen[i].VertrouwenInAndereMensen = Number(vertrouwen[i].VertrouwenInAndereMensen);
-  // };
-
   for(var i = 0; i < vertrouwen.length; i ++){
     delete(vertrouwen[i].ID)
     delete(vertrouwen[i].Migratieachtergrond)
@@ -40,63 +28,6 @@ function importData(error, response){
     vertrouwen[i].TweedeKamer = Number(vertrouwen[i].TweedeKamer);
     vertrouwen[i].VertrouwenInAndereMensen = Number(vertrouwen[i].VertrouwenInAndereMensen);
   };
-
-  // console.log('vertrouwen',vertrouwen)
-  // console.log('keys',Object.keys(vertrouwen[0]))
-  // console.log('values',Object.values(vertrouwen[0]))
-
-
-  // totaal[years[0]] = {};
-  // totaal[years[0]][Object.keys(vertrouwen[0])[0]] = Object.values(vertrouwen[0])[0]
-  // totaal[years[0]][Object.keys(vertrouwen[0])[1]] = Object.values(vertrouwen[0])[1]
-  // totaal[years[0]][Object.keys(vertrouwen[0])[2]] = Object.values(vertrouwen[0])[2]
-  // totaal[years[0]][Object.keys(vertrouwen[0])[3]] = Object.values(vertrouwen[0])[3]
-  // totaal[years[0]][Object.keys(vertrouwen[0])[4]] = Object.values(vertrouwen[0])[4]
-  // totaal[years[0]][Object.keys(vertrouwen[0])[5]] = Object.values(vertrouwen[0])[5]
-  // totaal[years[0]][Object.keys(vertrouwen[0])[6]] = Object.values(vertrouwen[0])[6]
-  // totaal[years[0]][Object.keys(vertrouwen[0])[7]] = Object.values(vertrouwen[0])[7]
-  //
-  // totaal[years[1]] = {};
-  // totaal[years[1]][Object.keys(vertrouwen[1])[0]] = Object.values(vertrouwen[1])[0]
-  // totaal[years[1]][Object.keys(vertrouwen[1])[1]] = Object.values(vertrouwen[1])[1]
-  // totaal[years[1]][Object.keys(vertrouwen[1])[2]] = Object.values(vertrouwen[1])[2]
-  // totaal[years[1]][Object.keys(vertrouwen[1])[3]] = Object.values(vertrouwen[1])[3]
-  // totaal[years[1]][Object.keys(vertrouwen[1])[4]] = Object.values(vertrouwen[1])[4]
-  // totaal[years[1]][Object.keys(vertrouwen[1])[5]] = Object.values(vertrouwen[1])[5]
-  // totaal[years[1]][Object.keys(vertrouwen[1])[6]] = Object.values(vertrouwen[1])[6]
-  // totaal[years[1]][Object.keys(vertrouwen[1])[7]] = Object.values(vertrouwen[1])[7]
-  //
-  // totaal[years[5]] = {};
-  // totaal[years[5]][Object.keys(vertrouwen[5])[0]] = Object.values(vertrouwen[5])[0]
-  // totaal[years[5]][Object.keys(vertrouwen[5])[1]] = Object.values(vertrouwen[5])[1]
-  // totaal[years[5]][Object.keys(vertrouwen[5])[2]] = Object.values(vertrouwen[5])[2]
-  // totaal[years[5]][Object.keys(vertrouwen[5])[3]] = Object.values(vertrouwen[5])[3]
-  // totaal[years[5]][Object.keys(vertrouwen[5])[4]] = Object.values(vertrouwen[5])[4]
-  // totaal[years[5]][Object.keys(vertrouwen[5])[5]] = Object.values(vertrouwen[5])[5]
-  // totaal[years[5]][Object.keys(vertrouwen[5])[6]] = Object.values(vertrouwen[5])[6]
-  // totaal[years[5]][Object.keys(vertrouwen[5])[7]] = Object.values(vertrouwen[5])[7]
-
-
-  // nietWesters = {};
-  // nietWesters[years[5]] = {}; // dit geeft die error
-  // nietWesters[years[5]][Object.keys(vertrouwen[23])[0]] = Object.values(vertrouwen[23])[0]
-  // nietWesters[years[5]][Object.keys(vertrouwen[23])[1]] = Object.values(vertrouwen[23])[1]
-  // nietWesters[years[5]][Object.keys(vertrouwen[23])[2]] = Object.values(vertrouwen[23])[2]
-  // nietWesters[years[5]][Object.keys(vertrouwen[23])[3]] = Object.values(vertrouwen[23])[3]
-  // nietWesters[years[5]][Object.keys(vertrouwen[23])[4]] = Object.values(vertrouwen[23])[4]
-  // nietWesters[years[5]][Object.keys(vertrouwen[23])[5]] = Object.values(vertrouwen[23])[5]
-  // nietWesters[years[5]][Object.keys(vertrouwen[23])[6]] = Object.values(vertrouwen[23])[6]
-  // nietWesters[years[5]][Object.keys(vertrouwen[23])[7]] = Object.values(vertrouwen[23])[7]
-
-  // for(var i = 0; i < vertrouwen.length; i ++){
-  //   for(var j = 0; j < years.length; j ++){
-  //     totaal[years[j]] = {};
-  //     for(var k = 0; k < 8; k ++){
-  //       // console.log('j:', j, 'i:', i, 'k', k)
-  //       totaal[years[j]][Object.keys(vertrouwen[i])[k]] = Object.values(vertrouwen[i])[k]
-  //     }
-  //   }
-  // }
 
   // create an array with all variables from vertrouwen dataset
   var vertrouwenVars = Object.keys(vertrouwen[0])
@@ -138,8 +69,6 @@ function importData(error, response){
   vertrouwen["nederlands"] = nederlands
   vertrouwen["westers"] = westers
   vertrouwen["nietWesters"] = nietWesters
-
-  console.log('vertrouwendata',vertrouwen)
 
   // wat je kan doen om niet het aantal variabelen te hardcoden is de variabelen van de desbetreffende dict in een array pushen met objectkeys functie en daar dan de lengte van nemen
 
@@ -189,7 +118,7 @@ function importData(error, response){
     }
   }
 
-  dienstverlening = {};
+  var dienstverlening = {};
   dienstverlening["totaal"] = totaal
   dienstverlening["nederlands"] = nederlands
   dienstverlening["westers"] = westers
@@ -210,10 +139,7 @@ function importData(error, response){
     faciliteiten[i].LaptopOfNetbook = Number(faciliteiten[i].LaptopOfNetbook);
   };
 
-  console.log('faciliteiten', faciliteiten)
-
   var faciliteitenVars = Object.keys(faciliteiten[0])
-  console.log('vars',faciliteitenVars.length)
 
   var totaal = {};
   for(var i = 0; i < years.length; i ++){
@@ -247,13 +173,11 @@ function importData(error, response){
     }
   }
 
-  faciliteiten = {};
+  var faciliteiten = {};
   faciliteiten["totaal"] = totaal
   faciliteiten["nederlands"] = nederlands
   faciliteiten["westers"] = westers
   faciliteiten["nietWesters"] = nietWesters
-
-  // console.log('fac', faciliteiten)
 
   var gebruik = (bigDatty[3]["gebruik"]);
 
@@ -270,10 +194,7 @@ function importData(error, response){
     gebruik[i].MinderDanEenKeerPerWeek = Number(gebruik[i].MinderDanEenKeerPerWeek);
   };
 
-  console.log('gebruik', gebruik)
-
   var gebruikVars = Object.keys(gebruik[0])
-  console.log('vars',gebruikVars.length)
 
   var totaal = {};
   for(var i = 0; i < years.length; i ++){
@@ -307,13 +228,11 @@ function importData(error, response){
     }
   }
 
-  gebruik = {};
+  var gebruik = {};
   gebruik["totaal"] = totaal
   gebruik["nederlands"] = nederlands
   gebruik["westers"] = westers
   gebruik["nietWesters"] = nietWesters
-
-  console.log('geb', gebruik)
 
 
   var interesse = (bigDatty[4]["interesse"]);
@@ -328,10 +247,8 @@ function importData(error, response){
     interesse[i].NietGeinteresseerd = Number(interesse[i].NietGeinteresseerd);
   };
 
-  console.log("intrs", interesse)
 
   var interesseVars = Object.keys(interesse[0])
-  console.log('varsI', interesseVars.length)
 
   var totaal = {};
   for(var i = 0; i < years.length; i ++){
@@ -365,19 +282,17 @@ function importData(error, response){
     }
   }
 
-  interesse = {};
+  var interesse = {};
   interesse["totaal"] = totaal
   interesse["nederlands"] = nederlands
   interesse["westers"] = westers
   interesse["nietWesters"] = nietWesters
 
-  console.log('geb', gebruik)
-
-
   var participatie = (bigDatty[5]["participatie"]);
 
   for(var i = 0; i < participatie.length; i ++){
-    participatie[i].ID = Number(participatie[i].ID);
+    delete(participatie[i].ID)
+    delete(participatie[i].Migratieachtergrond)
     participatie[i].Periode = Number(participatie[i].Periode);
     participatie[i].RadioTelevisieOfKrantIngeschakeld = Number(participatie[i].RadioTelevisieOfKrantIngeschakeld);
     participatie[i].PolitiekeOrganisatieIngeschakeld = Number(participatie[i].PolitiekeOrganisatieIngeschakeld);
@@ -390,8 +305,51 @@ function importData(error, response){
     participatie[i].Anders = Number(participatie[i].Anders);
   };
 
+  var participatieVars = Object.keys(participatie[0])
 
-  // console.log(vertrouwen)
+  var totaal = {};
+  for(var i = 0; i < years.length; i ++){
+    totaal[years[i]] = {};
+    for(var j = 0; j < participatieVars.length; j ++){
+      totaal[years[i]][Object.keys(participatie[i])[j]] = Object.values(participatie[i])[j]
+    }
+  }
+
+  var nederlands = {};
+  for(var i = 0; i < years.length; i ++){
+    nederlands[years[i]] = {};
+    for(var j = 0; j < participatieVars.length; j ++){
+      nederlands[years[i]][Object.keys(participatie[i+6])[j]] = Object.values(participatie[i+6])[j]
+    }
+  }
+
+  var westers = {};
+  for(var i = 0; i < years.length; i ++){
+    westers[years[i]] = {};
+    for(var j = 0; j < participatieVars.length; j ++){
+      westers[years[i]][Object.keys(participatie[i+12])[j]] = Object.values(participatie[i+12])[j]
+    }
+  }
+
+  var nietWesters = {};
+  for(var i = 0; i < years.length; i ++){
+    nietWesters[years[i]] = {};
+    for(var j = 0; j < participatieVars.length; j ++){
+      nietWesters[years[i]][Object.keys(participatie[i+18])[j]] = Object.values(participatie[i+18])[j]
+    }
+  }
+
+  participatie = {};
+  participatie["totaal"] = totaal
+  participatie["nederlands"] = nederlands
+  participatie["westers"] = westers
+  participatie["nietWesters"] = nietWesters
+
+  console.log('vertrouwen', vertrouwen)
+  console.log('faciliteiten', faciliteiten)
+  console.log('gebruik', gebruik)
+  console.log('interesse', interesse)
+  console.log('participatie', participatie)
 
   makeLinegraphCanvas(vertrouwen)
 
